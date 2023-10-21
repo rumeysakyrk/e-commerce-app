8da7fdd7dbdff60a304b92bb8ed3d80364ae2ad5
rumeysakyrk <rumeysakyrk@gmail.com>
2023-10-21 17:31:44 +0300

    product layout grid

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Card from "../../components/Card";
+import {Grid } from '@chakra-ui/react'
 
 function Products() {
   return (
     <div>
-      
-      <Card />
+        <Grid templateColumns='repeat(5, 1fr)' gap={6}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </Grid>
     </div>
   )
 }
