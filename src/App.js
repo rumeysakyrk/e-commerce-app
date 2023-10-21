bd182be1da816f866ac6737c662a32b5cab63cc6
rumeysakyrk <rumeysakyrk@gmail.com>
2023-10-21 15:59:22 +0300

    added Chakra UI and home page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+    <div>
+      <Navbar />
+      <div id="content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </div>
     </div>
+  </Router>
   );
 }
 
+function Home(){
+  return <h2>Home</h2>
+}
 export default App;
