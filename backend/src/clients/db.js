5bfaf8587e155982cea90abc42efa5010f977619
rumeysakyrk <rumeysakyrk@gmail.com>
2023-10-21 14:19:42 +0300

    init

---
+++ backend/src/clients/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB: Connectted'))
+  .catch((err) => console.log(err.message));
