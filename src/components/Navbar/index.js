15e86c72dd6d30aa2e0f0b4ec28669ded41de5c3
rumeysakyrk <rumeysakyrk@gmail.com>
2023-10-28 14:25:10 +0300

    some css changes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,15 +7,16 @@ import { useCart } from '../../contexts/CartContext';
 
 function Navbar() {
     const { loggedIn, user } = useAuth();
-    const {items} =useCart();
+    const { items } = useCart();
 
     console.log(loggedIn);
 
     return (
         <nav className={styles.nav}>
             <div className={styles.left}>
-                <div className="logo">
-                    <Link to="/">rbkCommerce😁</Link>
+                <div className={styles.logo}>
+                    <img src='https://images.unsplash.com/photo-1685062428503-e295f50850f6?auto=format&fit=crop&q=80&w=2958&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="Logo" />
+                    <Link to="/">e-Commerce </Link>
                 </div>
 
                 <ul className={styles.menu}>
@@ -36,28 +37,28 @@ function Navbar() {
                     </>
                 )}
 
-                 { user && user.role ==='admin' && (
+                {user && user.role === 'admin' && (
                     <Link to="/admin">
                         <Button colorScheme='purple' variant={"ghost"} >Admin</Button>
                     </Link>
                 )
 
                 }
-              
+
 
                 {loggedIn && (
                     <>
-                    {
-                        items.length > 0 &&(
-                            <Link to="/cart">
-                            <Button colorScheme='purple' variant={"outline"}>
-                                Cart ({items.length})
-                            </Button>
-                            </Link>
-                        )
-                    }
+                        {
+                            items.length > 0 && (
+                                <Link to="/cart">
+                                    <Button colorScheme='purple' variant={"outline"}>
+                                        Cart ({items.length})
+                                    </Button>
+                                </Link>
+                            )
+                        }
 
-                     <Link to="/profile">
+                        <Link to="/profile">
                             <Button colorScheme='purple'>Profile</Button>
                         </Link>
                     </>
